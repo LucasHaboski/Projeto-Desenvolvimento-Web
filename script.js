0a81d6250e480b2587860becba74cea5b6d00eff
GEnacheC <guilherme.enache@gmail.com>
2024-10-26 12:19:18 -0300

    Modularizando arquivos corretamente + Adicionando features finais de login

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const roles = {
-    ADMIN: "admin",
-    DEFAULT: "default"
-}
+  ADMIN: "admin",
+  DEFAULT: "default",
+};
 
 const usersData = [
-    { username: "admin", password: "pass", role: roles.ADMIN },
-    { username: "user 1", password: "pass", role: roles.DEFAULT },
-    { username: "user 2", password: "pass", role: roles.DEFAULT },
-]
+  { username: "admin", password: "pass", role: roles.ADMIN },
+  { username: "user 1", password: "pass", role: roles.DEFAULT },
+  { username: "user 2", password: "pass", role: roles.DEFAULT },
+];
 
-sessionStorage.setItem("users", JSON.stringify(usersData))
+sessionStorage.setItem("users", JSON.stringify(usersData));
 
 const user = JSON.parse(sessionStorage.getItem("user"));
 
